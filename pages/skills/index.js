49f28afea5b7f632b095d221c0b9deceb6bad6f6
rjhee <qkdrkwogml@gmail.com>
2023-01-09 10:29:55 +0900

    add styles, icon lib

---
+++ pages/skills/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div className='screen skills'>
+           <h1>skills</h1>
+        </div>
+    );
+};
+
+export default Skills;
---
+++ pages/workExperience/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const workExperience = () => {
+    return (
+        <div className='screen workExperience'>
+            <h1>workExperience</h1>
+        </div>
+    );
+};
+
+export default workExperience;
